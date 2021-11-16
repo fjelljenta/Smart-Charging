13e859d42c7ac631ef0e468de82679e5d2451d4f
jezerjojo14 <63692326+jezerjojo14@users.noreply.github.com>
2021-11-16 23:27:02 +0530

    added new changes in Max_k_cut_functions.py to MIS_functions.py

--- MIS_functions.py
+++ MIS_functions.py
@@ -88,9 +88,9 @@ def run_circuit(circ, param_list, nshots=512, simulator='qasm_simulator'):
     :return:  counts = dict{measurement result in string:count},
               transpiled_circ = The QuantumCircuit object corresponding to the transpiled circuit
     """
-    circ = circ.bind_parameters(param_list)
+    circ_w_param = circ.bind_parameters(param_list)
     backend = Aer.get_backend(simulator)
-    transpiled_circ = transpile(circ, backend)
+    transpiled_circ = transpile(circ_w_param, backend)
     counts = backend.run(transpiled_circ, shots=nshots).result().get_counts()
     return counts, transpiled_circ
 
@@ -114,7 +114,33 @@ def compute_cost(counts, l, w, n_counts=512):
     return average_cost
 
 
-def circuit_optimize_wrapper(circ, l, w, nshots=512, simulator='qasm_simulator'):
+def show_distribution(counts, l):
+    res = {}
+    for measurement, count in counts.items():
+        partition = [int(measurement[i:i + l], 2) for i in range(0, len(measurement), l)]
+        partition_corrected = []
+        group = []
+        added = []
+        for i in range(len(partition)):
+            if i not in added:
+                group.append(i)
+                added.append(i)
+                for j in range(i, len(partition)):
+                    if partition[i] == partition[j] and j not in added:
+                        group.append(j)
+                        added.append(j)
+            if len(group) != 0:
+                partition_corrected.append(group)
+            group = []
+        added = []
+        if str(partition_corrected) not in res.keys():
+            res[str(partition_corrected)] = count
+        else:
+            res[str(partition_corrected)] += count
+    return res
+
+
+def func_to_optimize_wrapper(circ, l, w, nshots=512, simulator='qasm_simulator'):
     """
     :param circ:  The QuantumCircuit object corresponding to the full circuit, without feeding parameters or transpiling
     :param l: The number of qubits representing a node
@@ -135,11 +161,100 @@ def circuit_optimize_wrapper(circ, l, w, nshots=512, simulator='qasm_simulator')
     return func_to_optimize
 
 
+def full_optimization_loop(n, l, w, p, bounds=[(-np.pi, np.pi), (0, 4*np.pi)], nshots=512, simulator='qasm_simulator',
+                           local_optimization_method='BFGS', optimal_cost=None):
+    """
+    :param n: The number of nodes
+    :param l: The number of qubits representing a node
+    :param w: The weight matrix for edges
+    :param p: The number of layers
+    :param bounds: The bounds for parameters, in the form of [(param_1_low, param_1_high),
+                                                              (param_2_low, param_2_high), ...]
+    :param nshots: The number of shots
+    :param simulator: The simulator
+    :param local_optimization_method: The local optimizer to use. By default, BFGS.
+                                      Other recommended choices include Powell, Nelder-Mead, COBYLA, ...
+                                      When set to None, no local optimization will be performed
+    :param optimal_cost: The optimal cost found by brute force. By default, None.
+    :return: three lists showing optimization history results, containing respectively
+             1. parameters in list format,
+             2. cost in float
+             3. ciruits run as Qiskit QuantumCircuit objects
+    """
+    ####################################################################################################################
+    # Initialize
+    backend = Aer.get_backend(simulator)
+    backend.shots = nshots
+    param_history = []
+    cost_history = []
+    circ_history = []
+    ####################################################################################################################
+    # Run the educated global guess (EGG) optimization for the first time
+    circ = make_full_circuit(n, l, w, 1)
+    circ_history.append(circ)
+    func_to_optimize = func_to_optimize_wrapper(circ, l, w)
+    result = differential_evolution(func_to_optimize, bounds)
+    param, cost = result.x, -1 * result.fun
+    print('1st params', param)
+    print('1st cost', cost)
+    param_history.append(param)
+    cost_history.append(cost)
+    ####################################################################################################################
+    # If depth = 1, no need to continue
+    if p < 2:
+        return param_history, cost_history, circ_history
+    ####################################################################################################################
+    # Else continue
+    for i in range(2, p+1):
+        if i == 2:
+            abbrev = 'nd'
+        else:
+            abbrev = 'th'
+    ####################################################################################################################
+    # Run the educated global guess (EGG) optimization for ith iteration
+        circ = make_full_circuit(n, l, w, i)
+        param_names = circ.parameters
+        param_bind_dict = {}
+        for j in range(i-1):
+            param_prev = param_history[-1]
+            param_bind_dict[param_names[j]] = param_prev[j]
+            param_bind_dict[param_names[j + i]] = param_prev[j + i - 1]
+        circ_w_param = circ.bind_parameters(param_bind_dict)
+        circ_history.append(circ_w_param)
+        func_to_optimize = func_to_optimize_wrapper(circ_w_param, l, w)
+        result = differential_evolution(func_to_optimize, bounds)
+        param, cost = result.x, -1 * result.fun
+        complete_param = np.concatenate((param_prev[:i-1], np.array([param[0]]),
+                                         param_prev[i-1:], np.array([param[1]])))
+        print(str(i) + abbrev + ' iteration (EGG), params', complete_param)
+        print(str(i) + abbrev + ' iteration (EGG),  cost', cost)
+        param_history.append(complete_param)
+        cost_history.append(cost)
+    ####################################################################################################################
+    # Run the local optimization of choice for ith iteration if neededs
+        if local_optimization_method is not None:
+            func_to_optimize = func_to_optimize_wrapper(circ, l, w)
+            result = minimize(func_to_optimize, complete_param, method=local_optimization_method)
+            param, cost = result.x, -1 * result.fun
+            print(str(i) + abbrev + ' iteration (' + local_optimization_method + '), params', param)
+            print(str(i) + abbrev + ' iteration (' + local_optimization_method + '), cost', cost)
+            param_history.append(param)
+            cost_history.append(cost)
+            circ_history.append(circ)
+    ####################################################################################################################
+    # If optimal cost found by brute force is provided, compute the approximation ratio evolution
+    if optimal_cost is not None:
+        print('Approximation Ratio Evolution ', cost_history / optimal_cost)
+    ####################################################################################################################
+    # lists of parameters, costs and quantum circuits are returned
+    return param_history, cost_history, circ_history
 
-# The classical methods of solving MIS have not been implemented
 
 
+# Classical methods of solving MIS not implemented yet
 
+#
+#
 # def Monte_Carlo_solver(G,k):
 #     """Classical Monte-carlo solution of the max-k-cut problem of Graph G
 #     Args:
@@ -339,10 +454,10 @@ def circuit_optimize_wrapper(circ, l, w, nshots=512, simulator='qasm_simulator')
 #             P_opt = P_try.copy()
 #             C_opt = C_try
 #     return C_opt, P_opt
-#
-# def benchmarking(function, *args):
-#     start = dt.datetime.now()
-#     result = function(*args)
-#     stop = dt.datetime.now()
-#     passed_time = stop-start
-#     return result, passed_time
+
+def benchmarking(function, *args):
+    start = dt.datetime.now()
+    result = function(*args)
+    stop = dt.datetime.now()
+    passed_time = stop-start
+    return result, passed_time
