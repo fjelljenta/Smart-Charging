bc33d1c78f29364b8bcf3d6dac8dfd4fb829f141
jezerjojo14 <63692326+jezerjojo14@users.noreply.github.com>
2021-11-18 04:19:15 +0530

    web app more or less done

--- web_app/console_simulation/mc_functions.py
+++ web_app/console_simulation/mc_functions.py
@@ -1,8 +1,45 @@
 import random
+from console_simulation.data_processing import *
+# from console_simulation.Max_k_cut_classical_functions import *
+from console_simulation.Max_k_cut_quantum_functions import *
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import minimize
+
+# Set parameters here
+
+p = 3  # the depth of QAOA
+
+l = 2  # the number of qubits for each node, k = 2^l where k is the number of subgraphs we'd like to cut,
+       # or to say, the k in 'max-k-cut'. l = 2 for a max-4-cut example
+
+init_params = [np.pi/8, np.pi]*p
+
+local_optimization_method = 'Powell'
+
+k = 2**l
 
 def init_vehicles(n):
-    d={"vehicles": [{"importance": round(0.1+0.9*random.random(), 3), "charging_time": round(5*(0.1+0.9*random.random()), 2)} for _ in range(n)]}
+    d={"vehicles": [{"importance": round(0.1+0.9*random.random(), 3), "charging_time": round(20*(0.1+0.9*random.random()), 2)} for _ in range(n)]}
     return d
 
 def get_mc(jobs):
-    return [int(4*random.random()) for _ in jobs]
+    weights=[]
+    for i in range(len(jobs)):
+        row=[]
+        for j in range(len(jobs)):
+            row+=[min(float(jobs[i]["importance"])*float(jobs[j]["charging_time"]), float(jobs[j]["importance"])*float(jobs[i]["charging_time"]))]
+        weights+=[row]
+    R = compute_normalization_scale(weights)
+    rescaled_weights = weights / R
+    print("get_mc called")
+    circ = make_full_circuit(5,l,rescaled_weights,p)
+    simulator='qasm_simulator'
+    backend = Aer.get_backend(simulator)
+    backend.shots = 128
+    circ_w_param = circ.bind_parameters([ 2.95085482e+00, -1.25155070e-01, -4.59385172e-03,  7.15978099e+00, 1.03754388e+01,  2.32943974e+00])
+    transpiled_circ = transpile(circ_w_param, backend)
+    counts = backend.run(transpiled_circ, shots=128).result().get_counts()
+    measurement=max(counts, key=counts.get)
+    return [int(measurement[i:i + l], 2) for i in range(0, len(measurement), l)]
--- web_app/console_simulation/mis_functions.py
+++ web_app/console_simulation/mis_functions.py
@@ -1,4 +1,5 @@
 import random
+from console_simulation.Maximal_independent_set_quantum_functions import *
 
 def init_jobs(n):
     d={}
@@ -7,10 +8,47 @@ def init_jobs(n):
     return d
 
 def get_mis(jobs):
-    mis=[]
-    for company in jobs.keys():
-        if random.random()>0.3:
-            index=1+int(len(jobs[company])*random.random())
-            mis+=[{"job-ID": "job-"+company[-1].upper()+str(index), "start_time": jobs[company][index-1]["start_time"]}]
-    mis.sort(key = lambda el : el["start_time"])
-    return {"mis": [el["job-ID"] for el in mis]}
+    # mis=[]
+    # for company in jobs.keys():
+    #     if random.random()>0.3:
+    #         index=1+int(len(jobs[company])*random.random())
+    #         mis+=[{"job-ID": "job-"+company[-1].upper()+str(index), "start_time": jobs[company][index-1]["start_time"]}]
+    # mis.sort(key = lambda el : el["start_time"])
+
+    p = 2  # the depth of QAOA
+    ng = 4  # number_of_groups
+    n = 10  # total number of nodes, or vehicles
+    mt = 500  # max_time, Start time charging limit (say, after 100 minutes, every car started at least charging)
+    mct = 100  # max_charging_time, maximum time a car can charge (like 6h)
+    U = 10  # The size of punishment added to the cost due to including nodes of the same group in the MIS
+
+    j=[]
+
+    for ch in ['a','b','c','d']:
+        j+=[{**job, **{"company": "company-"+ch, "index": index}} for index, job in enumerate(jobs["company-"+ch])]
+
+    adj = []
+
+    for i in range(10):
+        row=[]
+        for k in range(10):
+            row += [1] if i!=k and (j[i]["company"]==j[k]["company"] or min(float(j[i]["start_time"])+float(j[i]["duration"]), float(j[k]["start_time"])+float(j[k]["duration"]))>max(float(j[i]["start_time"]), float(j[k]["start_time"]))) else [0]
+        adj+=[row]
+
+    print(adj)
+
+    circ = make_full_circuit_MIS(n,adj,p)
+    simulator='qasm_simulator'
+    backend = Aer.get_backend(simulator)
+    backend.shots = 128
+    circ_w_param = circ.bind_parameters([ 0.77465785, 2.82478591, -1.13516493, 3.90552469])
+    transpiled_circ = transpile(circ_w_param, backend)
+    counts = backend.run(transpiled_circ, shots=128).result().get_counts()
+    measurement=max(counts, key=counts.get)
+
+    preprocessed_chosen_set = np.argwhere(np.array(list(measurement))=='1')
+    chosen_set = np.concatenate(preprocessed_chosen_set)
+
+    return {"mis": ["job-"+j[pos]["company"][-1].upper()+str(j[pos]["index"]+1) for pos in chosen_set]}
+
+    # return {"mis": [el["job-ID"] for el in mis]}
