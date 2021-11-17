36ff5b6a6e07c97765d6a337c2c0f431e2cae32e
Catharina <93005697+CatharinaBroocks@users.noreply.github.com>
2021-11-17 23:49:46 +0100

    final added functions and descriptions

--- Code/Max_k_cut_classical_functions.py
+++ Code/Max_k_cut_classical_functions.py
@@ -2,8 +2,10 @@ from Code.Max_k_cut_quantum_functions import *
 from Code.data_processing import *
 from itertools import combinations
 import numpy as np
+import matplotlib.pyplot as plt
 import copy
 import ast
+import json
 
 
 def powerset(l):
@@ -64,7 +66,7 @@ def show_distribution(counts, l):
     return res
 
 
-def Monte_Carlo_solver(G, k):
+def Monte_Carlo_solver(G, k, print_progress=False):
     """Classical Monte-carlo solution of the max-k-cut problem of Graph G
     Args:
         G (graph): Graph of the max-k-cut problem
@@ -84,7 +86,7 @@ def Monte_Carlo_solver(G, k):
         P_opt['P' + str(i)] = []
 
     for j in range(N):
-        P_opt['P' + str(np.random.randint(k))].append(j)  # maybe change to actual nodes the graph
+        P_opt['P' + str(np.random.randint(k))].append(j)
 
     C_opt = cost(G, P_opt)
     f = 0
@@ -113,14 +115,17 @@ def Monte_Carlo_solver(G, k):
             f = 0
             P_opt = copy.deepcopy(P)
             C_opt = C.copy()
-            print('C_opt=', C_opt, '  P_opt=', P_opt, '\n')
+            if print_progress:
+                print('C_opt=', C_opt, '  P_opt=', P_opt)
         else:
             f += 1
-
+            
+    print('C_opt_ms=', C_opt, '  P_opt_ms=', P_opt)
     return C_opt, P_opt
 
 
-def brut_force(G, k):
+
+def brut_force(G, k, print_progress=False):
     """Classical brut-force solution of the max-k-cut problem of Graph G
     Args:
         G (graph): Graph of the max-k-cut problem
@@ -147,8 +152,10 @@ def brut_force(G, k):
     time = 0
 
     C_opt, P_opt = rec_cost_optimization(G, k, N, N_rec, P_opt, C_opt)
-    print('{}\r'.format('progress ' + str(100) + '%' + '   '), end="")
-
+    if print_progress:
+        print('{}\r'.format('progress ' + str(100) + '%' + '   '), end="")
+    
+    print('C_opt=', C_opt, '  P_opt=', P_opt)
     return C_opt, P_opt
 
 
@@ -263,6 +270,14 @@ def rec_cost_optimization(G, k, N, N_rec, P_opt, C_opt):
 
 
 def benchmarking(function, *args):
+    """Record of the time a certain function call takes
+    Args:
+        function (function): function that should be benchmarked
+        *args: argument(s) of the function
+    Returns:
+        result: result of function evaluation
+        passed_time(datetime): time the function call took
+    """
     start = dt.datetime.now()
     result = function(*args)
     stop = dt.datetime.now()
@@ -271,16 +286,29 @@ def benchmarking(function, *args):
 
 
 def str_array_into_dict(P_str_array):
+    """transforms an array in string format into a dictionary
+    Args:
+        P_str_array (str): array transformed into a string
+    Returns:
+        P_dict (dict): array turned into a dictionary with keys P_i and values as the subarrays of the given string-array
+    """
     P_dict = {}
     participation_data = ast.literal_eval(P_str_array)
     P_len = len(participation_data)
-    for i in range(P_len):  # Key muss array sein, kein string
+    for i in range(P_len): 
         P_dict["P" + str(i)] = participation_data[i]
     return P_dict
 
 
 
 def plot_distribution_diagramm(G, distribution_qaoa):
+    """bar plot of the cost of a given partition on the x-axis and the number of counts during the qaoa at the y-axis
+    Args:
+        G (graph): Graph the partitions of the distribution_qaoa belong to 
+        distribution_qaoa (dict): dictionary with partitions as keys and corresponding 
+                                   number of counts of these partitions during the qaoa
+    Returns:
+    """
     count_list_qaoa = [[], []]
     for key, value in distribution_qaoa.items():
         P = str_array_into_dict(key)
@@ -294,6 +322,15 @@ def plot_distribution_diagramm(G, distribution_qaoa):
 
 
 def qaoa_highest_counts(distribution_qaoa, number_highest_counts):
+    """ filters for the Partitions with the highest number of counts during the qaoa
+    Args:
+        distribution_qaoa (dict): dictionary with partitions as keys and corresponding 
+                                   number of counts of these partitions during the qaoa
+        number_highest_counts (int): number of partitions with the highest counts that should be filtered                            
+    Returns:
+        highest_distr_qaoa (dict): distribution_qaoa in with only the 'number_highest_counts' many partitions 
+                                   with the highest counts are kept
+    """
     count_list = list(distribution_qaoa.values())
     top_indices = np.argsort(count_list)[-number_highest_counts:]
     top_counts = [count_list[i] for i in top_indices]
@@ -304,3 +341,98 @@ def qaoa_highest_counts(distribution_qaoa, number_highest_counts):
             highest_distr_qaoa[key] = value
 
     return highest_distr_qaoa
+
+
+
+def convert(o):
+    """ convertion needed for writing in json files
+    Args:
+        o: argument to be converted                          
+    Returns:
+        converts to format compatible with json
+    """
+    return o.item()  
+
+
+def general_benchmark_for_N(N, p_array=[3], repititions=5, k_array=[5]):
+    """ general benchmark for a given number of nodes and different qaoa-depths, number of cuts and repititions.
+        All created data is saved in json files in the 'create_data' folder
+    Args:
+        N (int): number of nodes the created graphs should have
+        p_array (array): array with qaoa depth-values
+        repititions (int): number of repititions for a given combination of N,p and k
+        k_array (array): number of cuts which should be done in the max-k-cut
+    Returns:
+        qaoa_benchmark_dict (dict): dictionary with str(p)+str(N) as keys and arrays of data sets with optimal cost and 
+                                    optimal partition for qaoa, monte-carlo and brute force for each repitition as values
+    """
+    qaoa_benchmark_dict={}
+    for k in k_array: 
+        for p in p_array:
+            for rep in range(repititions):
+                n_imp = np.random.randint(N+1) 
+                n_unimp = N-n_imp
+            
+                filename = "p_"+str(p)+"_N_"+str(N)+"_repetiton_"+str(rep)
+                graph_name = "graph_"+filename
+                data_name = "data_"+filename
+            
+                G = generate_data(n_unimp, n_imp, True, graph_name)
+                C_opt_qaoa, P_opt_qaoa, distribution_qaoa, param_history = qaoa_solver(G,k,p)   #change in a way that parameter and distribution_qaoa are returned as well
+                C_opt_mc, P_opt_mc =Monte_Carlo_solver(G,k)
+                C_opt, P_opt=brut_force(G,k)
+            
+                key=str(p)+str(N)
+                if rep==0:
+                    qaoa_benchmark_dict[key]=[[C_opt_qaoa, P_opt_qaoa, C_opt_mc, P_opt_mc, C_opt, P_opt]]
+                else:
+                    qaoa_benchmark_dict[key].append([C_opt_qaoa, P_opt_qaoa, C_opt_mc, P_opt_mc, C_opt, P_opt])
+                         
+            
+                with open("created_data/"+data_name+".json", "wb") as f:
+                    all_data = {}
+                    all_data["n_imp"] = n_imp
+                    all_data["n_unimp"] = n_unimp
+                    all_data["C_opt_qaoa"] = C_opt_qaoa
+                    all_data["P_opt_qaoa"] = P_opt_qaoa
+                    all_data["C_opt_mc"] = C_opt_mc
+                    all_data["P_opt_mc"] = P_opt_mc
+                    all_data["C_opt"] = C_opt
+                    all_data["P_opt"] = P_opt
+                    all_data["distribution_qaoa"] = distribution_qaoa
+                    #all_data["param_history"] = param_history
+
+                    f.write(json.dumps(all_data, default=convert).encode("utf-8"))
+    
+    return qaoa_benchmark_dict  
+
+
+
+def qaoa_benchmark_plot_data_N(qaoa_benchmark_dict):
+    """ computes approximation ratios for given optimal solutions of qaoa and monte-carlo
+    Args:
+        qaoa_benchmark_dict (dict): dictionary containing optimal partitions and costs computed by qaoa, monte carlo and brute-force                          
+    Returns:
+        return p_values (array): array containing quoa depths p
+        approximation_ratio_qaoa (array): approximation ratios of qaoa for different depths p
+        approximation_ratio_mc (array): approximation ratios of monte-carlo (for different depths p, makes no difference for monte-carlo)
+    """
+    p_values=[]
+    approximation_ratio_qaoa=[]
+    approximation_ratio_mc=[]
+
+    for key, value in qaoa_benchmark_dict.items():   
+        approximation_qaoa=0
+        approximation_mc=0
+        for repitition in value:
+            C_qaoa=repitition[0]
+            C_mc=repitition[2]
+            C_opt=repitition[4]
+            approximation_qaoa += 100*C_qaoa/(1.0*C_opt)
+            approximation_mc += 100*C_mc/(1.0*C_opt)
+        
+        p_values.append(key[0])# look if 0 or 1    
+        approximation_ratio_qaoa.append(approximation_qaoa/5.0)
+        approximation_ratio_mc.append(approximation_mc/5.0)
+    
+    return p_values, approximation_ratio_qaoa, approximation_ratio_mc
